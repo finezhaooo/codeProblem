d4984119c561aec5d6891bb52f7be8f4d80db63b
finezhaooo <635388416@qq.com>
2022-07-01 17:31:34 +0800

    1的数量

--- commonDS/NumberOf1.java
+++ commonDS/NumberOf1.java
@@ -5,4 +5,13 @@
  * @Date: 2022/6/30/22:17
  */
 public class NumberOf1 {
+    public int numberOf1(int n) {
+        int count = 0;
+        while (n != 0) {
+            // n-1表示把第一个1之后的所有0变为1,当前1变为0
+            n = n & (n - 1);
+            count++;
+        }
+        return count;
+    }
 }
