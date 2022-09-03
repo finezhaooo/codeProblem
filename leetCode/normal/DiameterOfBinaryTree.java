98e22151173f9a2c2994be8a8cdc37ea072e3866
finezhaooo <635388416@qq.com>
2022-09-03 20:32:51 +0800

    543. 二叉树的直径

--- leetCode/normal/DiameterOfBinaryTree.java
+++ leetCode/normal/DiameterOfBinaryTree.java
@@ -1,8 +1,35 @@
-package normal;/**
+package normal;
+
+import dependency.TreeNode;
+
+/**
  * @ClassName: DiameterOfBinaryTree
  * @Description: 543. 二叉树的直径
  * @Author: zhaooo
  * @Date: 2022/9/3/14:56
  */
 public class DiameterOfBinaryTree {
+    int res = 0;
+
+    /**
+     * 模板解法
+     * @param root
+     * @return
+     */
+    public int diameterOfBinaryTree(TreeNode root) {
+        dfs(root);
+        return res;
+    }
+
+    public int dfs(TreeNode treeNode) {
+        if (treeNode == null) {
+            return 0;
+        }
+        int l = dfs(treeNode.left);
+        int r = dfs(treeNode.right);
+        // 更新全局变量
+        res = Math.max(res, l + r);
+        // 返回左右路径较长者
+        return Math.max(l, r) + 1;
+    }
 }
