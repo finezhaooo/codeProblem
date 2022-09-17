d5480bcf042b91b83f06f0d720cf3bbd8d13ff8c
finezhaooo <635388416@qq.com>
2022-09-17 11:38:54 +0800

    670. 最大交换

--- leetCode/normal/MaximumSwap.java
+++ leetCode/normal/MaximumSwap.java
@@ -1,5 +1,9 @@
+package normal;
+
+import java.util.Arrays;
+
 /**
- * @ClassName: MaximumSwap
+ * @ClassName: normal.MaximumSwap
  * @Description: 670. 最大交换
  * @Author: zhaooo
  * @Date: 2022/9/13/10:18
@@ -54,5 +58,46 @@ public class MaximumSwap {
         // 改回位置
         swap(i, j, nums);
         return num;
+    }
+
+    /**
+     * 贪心
+     * 将数字从大到小排列，与原数字比较，找出第一位置不一样的数。
+     * 如8217排序后变为8721，两两对比，第二个数不同，表示7和2交换，得到结果8712
+     * @param num
+     * @return
+     */
+    public int maximumSwap2(int num) {
+        // 排序数组元素
+        char[] orderNum = Integer.toString(num).toCharArray();
+        char[] oldNum = Integer.toString(num).toCharArray();
+        //这里是从小到大排列
+        Arrays.sort(orderNum);
+        int diff = -1;
+        // 比较第一个不同的元素
+        for (int i = 0; i < orderNum.length; i++) {
+            if (oldNum[i] != orderNum[orderNum.length - 1 - i]) {
+                diff = i;
+                break;
+            }
+        }
+        // 两数相同，不需要交换
+        if (diff == -1) {
+            return num;
+        }
+        // 两两交换
+        for (int i = oldNum.length - 1; i >= diff; i--) {
+            if (oldNum[i] == orderNum[orderNum.length - 1 - diff]) {
+                //交换后直接跳出
+                swap(oldNum, diff, i);
+                break;
+            }
+        }
+        return Integer.parseInt(new String(oldNum));
+    }
+    void swap(char[] chars, int lo, int hi) {
+        char tmp = chars[lo];
+        chars[lo] = chars[hi];
+        chars[hi] = tmp;
     }
 }
---
+++ leetCode/normal/RectangleAreaII.java
@@ -0,0 +1,8 @@
+package normal;/**
+ * @ClassName: RectangleAreaII
+ * @Description: 850. 矩形面积 II
+ * @Author: zhaooo
+ * @Date: 2022/9/16/9:59
+ */
+public class RectangleAreaII {
+}
