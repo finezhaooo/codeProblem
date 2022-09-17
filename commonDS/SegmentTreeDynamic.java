241f7aba967891f6b03f3c069e681442400f37da
finezhaooo <635388416@qq.com>
2022-09-17 11:42:41 +0800

    线段树（动态开点）

--- commonDS/SegmentTreeDynamic.java
+++ commonDS/SegmentTreeDynamic.java
@@ -1,41 +1,50 @@
 /**
  * @ClassName: SegmentTreeDynamic
  * @Description: 线段树（动态开点）
+ * 使用该数据结构的题:
+ *  MyCalendarI
+ *  RangeModule
+ *  RectangleAreaII
  * @Author: zhaooo
  * @Date: 2022/6/20/12:14
  */
 public class SegmentTreeDynamic {
     class Node {
         Node left, right;
+        // val是该节点的值，本代码中是该线段的总和
+        // add是懒标记即该点区间内每个点要更新的值
         int val, add;
     }
 
-    // 在区间 [start, end] 中更新区间 [l, r] 的结果
-    public void update(Node node, int start, int end, int l, int r, int val) {
-        // 要更新的区间在该点内部时懒更新
+    // 在区间 [l, r] 中更新区间 [start, end] 的结果
+    public void update(Node node, int start, int end, int l, int r, int add) {
+        // 要更新的区间（即[l, r]）大于该点（该点范围为[start, end]）时懒更新
         if (l <= start && end <= r) {
-            node.val += (end - start + 1) * val;
-            node.add = val;
+            node.val += (end - start + 1) * add;
+            //? 也可为node.add=add 因为原add要么为0，要么已经包含在node.val里
+            // 更新懒标记
+            node.add += add;
             return;
         }
+        // [l, r]不能完全包裹[start, end]
         int mid = (start + end) >> 1;
         // 开点和更新
         pushDown(node, mid - start + 1, end - mid);
         // 递归查询
         // [l,r]包含该点的左孩子
         if (l <= mid) {
-            update(node.left, start, mid, l, r, val);
+            update(node.left, start, mid, l, r, add);
         }
         // [l,r]包含该点的右孩子
         if (r > mid) {
-            update(node.right, mid + 1, end, l, r, val);
+            update(node.right, mid + 1, end, l, r, add);
         }
         // 向上更新结果
         pushUp(node);
     }
 
     public int query(Node node, int start, int end, int l, int r) {
-        //
+        // 该点（[start, end]）在查询区间（[l, r]）内
         if (l <= start && end <= r) {
             return node.val;
         }
@@ -68,6 +77,7 @@ public class SegmentTreeDynamic {
         if (node.add == 0) {
             return;
         }
+        // 对某个节点的懒标记进行下放时，左右儿子的标记也要更新，并且该节点的标记值要清零，以防重复下放
         node.left.val += node.add * leftNum;
         node.right.val += node.add * rightNum;
         node.left.add = node.add;
