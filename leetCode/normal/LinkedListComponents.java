1d40878d3b643b762652197536bc1b1d9848ce3e
finezhaooo <635388416@qq.com>
2022-11-12 17:00:36 +0800

    817. 链表组件

--- leetCode/normal/LinkedListComponents.java
+++ leetCode/normal/LinkedListComponents.java
@@ -1,5 +1,7 @@
 package normal;
 
+import dependency.ListNode;
+
 /**
  * @ClassName: LinkedListComponents
  * @Description: 817. 链表组件
@@ -7,4 +9,31 @@ package normal;
  * @Date: 2022/10/12/15:19
  */
 public class LinkedListComponents {
+    /**
+     * 简单模拟
+     * @param head
+     * @param nums
+     * @return
+     */
+    public int numComponents(ListNode head, int[] nums) {
+        boolean[] map = new boolean[(int) 1e4 + 10];
+        int ret = 0;
+        for (int num : nums) {
+            map[num] = true;
+        }
+        ListNode cur = head;
+        while (cur != null) {
+            if (map[cur.val]) {
+                ListNode tmp = cur.next;
+                while (tmp != null && map[tmp.val]) {
+                    tmp = tmp.next;
+                }
+                ret++;
+                cur = tmp;
+            } else {
+                cur = cur.next;
+            }
+        }
+        return ret;
+    }
 }
