359a7e919d34413c5ce45bd3275f034c61623263
finezhaooo <635388416@qq.com>
2023-03-18 14:26:27 +0800

    25. K 个一组翻转链表

--- leetCode/normal/ReverseNodesInKGroup.java
+++ leetCode/normal/ReverseNodesInKGroup.java
@@ -1,6 +1,6 @@
 package normal;
 
-import ListNode;
+import dependency.ListNode;
 
 /**
  * @ClassName: ReverseNodesInKGroup
@@ -21,30 +21,30 @@ public class ReverseNodesInKGroup {
      */
     public ListNode reverseKGroup(ListNode head, int k) {
         counter = k;
-        ListNode ret = reverseList(head,k);
+        ListNode ret = reverseList(head, k);
         // pre记录已反转部分尾节点
         ListNode pre = head;
         // 还有可以反转的部分
-        while(head.next!=null){
+        while (head.next != null) {
             counter = k;
             // pre记录已反转部分尾节点
             pre = head;
             // tmp记录未反转部分的头节点
             ListNode tmp = head.next;
             // 反转后tmp为已反转部分尾节点
-            head.next = reverseList(tmp,k);
+            head.next = reverseList(tmp, k);
             head = tmp;
         }
         // 最后一组不足k个则反转回去
-        pre.next = (counter==1?pre.next:reverseList(pre.next,k));
+        pre.next = (counter == 1 ? pre.next : reverseList(pre.next, k));
         return ret;
     }
 
-    
-    ListNode reverseList(ListNode head,int k){
+
+    ListNode reverseList(ListNode head, int k) {
         // cur记录已经反转部分的头节点
         ListNode cur = head;
-        while(k>1 && head.next!=null){
+        while (k > 1 && head.next != null) {
             ListNode tmp = head.next.next;
             head.next.next = cur;
             // head.next保存原链表cur的下一个，即未反转部分的头节点
