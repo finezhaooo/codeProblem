0c50c974e911c2844602f9aeb1af376ae53656e7
finezhaooo <finezhaooo@gmail.com>
2023-07-09 18:52:18 +0800

    67. 两数之和 II - 输入有序数组

---
+++ leetCode/normal/TwoSumIiInputArrayIsSorted.java
@@ -0,0 +1,55 @@
+package normal;
+
+/**
+ * @ClassName: TwoSumIiInputArrayIsSorted
+ * @Description: 167. 两数之和 II - 输入有序数组
+ * @Author: zhaooo
+ * @Date: 2023/07/09 14:20
+ */
+public class TwoSumIiInputArrayIsSorted {
+    /**
+     * 双指针
+     *
+     * @param numbers
+     * @param target
+     * @return
+     */
+    public int[] twoSum(int[] numbers, int target) {
+        int l = 0, r = numbers.length - 1;
+        while (l < r) {
+            int sum = numbers[l] + numbers[r];
+            if (sum == target) {
+                return new int[]{l + 1, r + 1};
+            } else if (sum < target) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+        return new int[0];
+    }
+
+    /**
+     * 二分
+     *
+     * @param numbers
+     * @param target
+     * @return
+     */
+    public int[] twoSum2(int[] numbers, int target) {
+        for (int i = 0; i < numbers.length; ++i) {
+            int low = i + 1, high = numbers.length - 1;
+            while (low <= high) {
+                int mid = (high - low) / 2 + low;
+                if (numbers[mid] == target - numbers[i]) {
+                    return new int[]{i + 1, mid + 1};
+                } else if (numbers[mid] > target - numbers[i]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
