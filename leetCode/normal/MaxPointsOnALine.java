3113c228302f8a32f0a30a81c0b9551505707822
finezhaooo <635388416@qq.com>
2023-09-02 12:59:27 +0800

    149. 直线上最多的点数

---
+++ leetCode/normal/MaxPointsOnALine.java
@@ -0,0 +1,72 @@
+package normal;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @ClassName: MaxPointsOnALine
+ * @Description: 149. 直线上最多的点数
+ * @Author: zhaooo
+ * @Date: 2023/09/02 10:48
+ */
+public class MaxPointsOnALine {
+    /**
+     * 暴力
+     *
+     * @param ps
+     * @return
+     */
+    public int maxPoints(int[][] ps) {
+        int n = ps.length;
+        int ans = 1;
+        for (int i = 0; i < n; i++) {
+            int[] x = ps[i];
+            for (int j = i + 1; j < n; j++) {
+                int[] y = ps[j];
+                int cnt = 2;
+                for (int k = j + 1; k < n; k++) {
+                    int[] p = ps[k];
+                    int s1 = (y[1] - x[1]) * (p[0] - y[0]);
+                    int s2 = (p[1] - y[1]) * (y[0] - x[0]);
+                    if (s1 == s2) cnt++;
+                }
+                ans = Math.max(ans, cnt);
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * 固定斜率
+     *
+     * @param points
+     * @return
+     */
+    public int maxPoints2(int[][] points) {
+        if (points.length <= 2) {
+            return points.length;
+        }
+        int n = points.length;
+        int ans = 1;
+        for (int i = 0; i < n; i++) {
+            Map<String, Integer> map = new HashMap<>();
+            // 由当前点 i 发出的直线所经过的最多点数量
+            int max = 0;
+            for (int j = i + 1; j < n; j++) {
+                int x1 = points[i][0], y1 = points[i][1], x2 = points[j][0], y2 = points[j][1];
+                int a = x1 - x2, b = y1 - y2;
+                int k = gcd(a, b);
+                // 斜率，避免保存浮点数
+                String key = (a / k) + "_" + (b / k);
+                map.put(key, map.getOrDefault(key, 0) + 1);
+                max = Math.max(max, map.get(key));
+            }
+            ans = Math.max(ans, max + 1);
+        }
+        return ans;
+    }
+
+    int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+}
