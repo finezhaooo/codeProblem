f4fc29a88af68eaee5970cafe86f8b0886179c35
finezhaooo <635388416@qq.com>
2023-12-29 19:43:09 +0800

    77. 组合

--- leetCode/normal/Combinations.java
+++ leetCode/normal/Combinations.java
@@ -31,16 +31,17 @@ public class Combinations {
         return res;
     }
 
-    void dfs(int start, int idx, Integer[] cur) {
-        if (idx == k) {
+    // 相当于每个长度时[start, n]选择一个 dfs继续
+    void dfs(int start, int len, Integer[] cur) {
+        if (len == k) {
             res.add(Arrays.asList(cur));
             return;
         }
         // i <= n - k + 1 + idx：剪枝
-        for (int i = start; i <= n - k + 1 + idx; i++) {
+        for (int i = start; i <= n + len - k + 1; i++) {
             Integer[] newList = cur.clone();
-            newList[idx] = i;
-            dfs(i + 1, idx + 1, newList);
+            newList[len] = i;
+            dfs(i + 1, len + 1, newList);
         }
     }
 
@@ -63,16 +64,48 @@ public class Combinations {
         return res;
     }
 
-    private void dfs(int n, int k, int index, Deque<Integer> path, List<List<Integer>> res) {
-        if (path.size() == k) {
-            res.add(new ArrayList<>(path));
+    private void dfs(int n, int k, int index, Deque<Integer> tmp, List<List<Integer>> res) {
+        if (tmp.size() == k) {
+            res.add(new ArrayList<>(tmp));
             return;
         }
-        for (int i = index; i <= n - (k - path.size()) + 1; i++) {
-            path.addLast(i);
-            dfs(n, k, i + 1, path, res);
+        for (int i = index; i <= n + tmp.size() - k + 1; i++) {
+            tmp.addLast(i);
+            dfs(n, k, i + 1, tmp, res);
             // 回溯
-            path.removeLast();
+            tmp.removeLast();
         }
+    }
+
+    /**
+     * 遍历
+     *
+     * @param n
+     * @param k
+     * @return
+     */
+    public List<List<Integer>> combine3(int n, int k) {
+        List<List<Integer>> ret = new ArrayList<>();
+        // 1<<n的每一位表示1到n
+        for (int i = 0; i < 1 << n; i++) {
+            int tmp = i;
+            int count = 0;
+            while (tmp != 0) {
+                // n-1表示把第一个1之后的所有0变为1,当前1变为0
+                tmp = tmp & (tmp - 1);
+                count++;
+            }
+            if (count == k) {
+                List<Integer> list = new ArrayList<>();
+                for (int j = 0; j < n; j++) {
+                    if (((1 << j) & i) > 0) {
+                        // 范围是[1, n]
+                        list.add(j + 1);
+                    }
+                }
+                ret.add(list);
+            }
+        }
+        return ret;
     }
 }
