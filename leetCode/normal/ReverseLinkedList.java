d163f8d91d41a5fcea36c3936808e580a9809075
finezhaooo <635388416@qq.com>
2024-01-05 17:06:52 +0800

    206. 反转链表

--- leetCode/normal/ReverseLinkedList.java
+++ leetCode/normal/ReverseLinkedList.java
@@ -20,13 +20,15 @@ public class ReverseLinkedList {
             return head;
         }
         ListNode newHead = reverseList(head.next);
+        // 通过当前节点（head）找到当前节点的前一个节点，并与之相连
         head.next.next = head;
+        // 与当前节点的下一个节点相连
         head.next = null;
         return newHead;
     }
 
     /**
-     * 双指针
+     * 双指针，动态改变链表每个指针的方向
      *
      * @param head
      * @return
@@ -41,5 +43,22 @@ public class ReverseLinkedList {
             cur = next;
         }
         return prev;
+    }
+
+    /**
+     * dummy节点
+     * @param head
+     * @return
+     */
+    public ListNode reverseList3(ListNode head) {
+        ListNode dummy = new ListNode();
+        ListNode tmp = dummy;
+        while (head != null) {
+            tmp = head.next;
+            head.next = dummy.next;
+            dummy.next = head;
+            head = tmp;
+        }
+        return dummy.next;
     }
 }
