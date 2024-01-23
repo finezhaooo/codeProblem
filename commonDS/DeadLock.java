8ce1fbac20a99de2079cf8210d2dcee382be1a3d
finezhaooo <635388416@qq.com>
2024-01-23 11:07:12 +0800

    死锁

---
+++ commonDS/DeadLock.java
@@ -0,0 +1,43 @@
+/**
+ * @ClassName: DeadLock
+ * @Description: 死锁
+ * @Author: zhaooo
+ * @Date: 2024/01/23 10:56
+ */
+public class DeadLock {
+    private static final Object resource1 = new Object();//资源 1
+    private static final Object resource2 = new Object();//资源 2
+
+    public static void main(String[] args) {
+        new Thread(() -> {
+            synchronized (resource1) {
+                System.out.println(Thread.currentThread() + "get resource1");
+                try {
+                    // 睡眠是为了确保对方得到第一把锁
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread() + "waiting get resource2");
+                synchronized (resource2) {
+                    System.out.println(Thread.currentThread() + "get resource2");
+                }
+            }
+        }, "线程 1").start();
+
+        new Thread(() -> {
+            synchronized (resource2) {
+                System.out.println(Thread.currentThread() + "get resource2");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread() + "waiting get resource1");
+                synchronized (resource1) {
+                    System.out.println(Thread.currentThread() + "get resource1");
+                }
+            }
+        }, "线程 2").start();
+    }
+}
