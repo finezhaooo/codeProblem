a89424db658f9879830ec1e149dd46c7f30cd50c
finezhaooo <635388416@qq.com>
2024-03-14 23:23:05 +0800

    3. 无重复字符的最长子串

--- leetCode/normal/LongestSubstringWithoutRepeatingCharacters.java
+++ leetCode/normal/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,7 +1,5 @@
 package normal;
 
-import java.util.Arrays;
-
 /**
  * @ClassName: LongestSubstringWithoutRepeatingCharacters
  * @Description: 3. 无重复字符的最长子串
@@ -11,24 +9,24 @@ import java.util.Arrays;
 public class LongestSubstringWithoutRepeatingCharacters {
     public int lengthOfLongestSubstring(String s) {
         int ans = 0;
-        // 126 - 32 + 1 = 95
-        // 126：~    32：空格
-        // 保存字符在s中的下标+1
+        // ASCII可打印字符范围为32-126 长度为95
+        // map记录字符上一次出现的序号，从1开始
         int[] map = new int[95];
-        Arrays.fill(map, -1);
-        int l = 0, r = 0;
-        for (; r < s.length(); r++) {
-            int idx = s.charAt(r) - 32;
-            // 当出现相同字符时才判断ans，减少判断次数
-            if (map[idx] != -1) {
-                // [l, r) : (r-1) - l + 1 = r - l
+        int l = 1, r = 1;
+        for (; r <= s.length(); r++) {
+            int idx = s.charAt(r - 1) - ' ';
+            // 当出现相同字符时才判断ans，因为长度在相同之前一直增加，减少判断次数
+            if (map[idx] != 0) {
+                // [1, l) [l, r) [r, len+1)
+                // 其中l和r对应的字符相同，求[l, r)的长度
                 ans = Math.max(ans, r - l);
-                // 有可能[map[idx], .., l, .., r]，即idx对应的元素的上一次出现在l之前
+                // 有可能[map[idx], ..., l, ..., r]，即idx对应的元素的上一次出现在l之前
                 l = Math.max(map[idx] + 1, l);
+                // 在r++之后会变为[l+1, r+1) 窗口内只有一个r
             }
             map[idx] = r;
         }
-        // r = s.length()
+        // r = len + 1
         ans = Math.max(ans, r - l);
         return ans;
     }
