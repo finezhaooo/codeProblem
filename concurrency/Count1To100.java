31e031fb5eebe2b025afaed5179813949604b939
finezhaooo <635388416@qq.com>
2024-03-31 16:22:11 +0800

    添加并发相关问题

---
+++ concurrency/Count1To100.java
@@ -0,0 +1,42 @@
+package concurrency;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @ClassName: concurrency.Count1To100
+ * @Description: 3个线程从0数到100
+ * @Author: zhaooo
+ * @Date: 2024/03/18 19:37
+ */
+public class Count1To100 {
+    static AtomicInteger i = new AtomicInteger(0);
+
+    public static void main(String[] args) {
+        new Thread(new Task(0)).start();
+        new Thread(new Task(1)).start();
+        new Thread(new Task(2)).start();
+    }
+
+    // 无锁编程
+    static class Task implements Runnable {
+        int idx;
+
+        Task(int idx) {
+            this.idx = idx;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                int k = i.get();
+                if (k > 100) {
+                    break;
+                }
+                if ((k % 3) == idx) {
+                    System.out.println(Thread.currentThread().getName() + "  " + k);
+                    i.getAndIncrement();
+                }
+            }
+        }
+    }
+}
---
+++ concurrency/ThreadSerial.java
@@ -0,0 +1,42 @@
+package concurrency;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @ClassName: concurrency.ThreadSerial
+ * @Description: 3个线程按照顺序执行
+ * @Author: zhaooo
+ * @Date: 2024/03/18 19:30
+ */
+public class ThreadSerial {
+    public static void main(String[] args) {
+        CountDownLatch latch1 = new CountDownLatch(1);
+        CountDownLatch latch2 = new CountDownLatch(1);
+        CountDownLatch latch3 = new CountDownLatch(1);
+        new Thread(new myWork(latch1, latch2)).start();
+        new Thread(new myWork(latch2, latch3)).start();
+        new Thread(new myWork(latch3, latch3)).start();
+        latch1.countDown();
+    }
+
+    static class myWork implements Runnable {
+        CountDownLatch latch1;
+        CountDownLatch latch2;
+
+        myWork(CountDownLatch latch1, CountDownLatch latch2) {
+            this.latch1 = latch1;
+            this.latch2 = latch2;
+        }
+
+        @Override
+        public void run() {
+            try {
+                latch1.await();
+                System.out.println(Thread.currentThread().getName() + " doing something");
+                latch2.countDown();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
