e2e0c7d955a7658a0ca090118639d5dacbd2e8f9
finezhaooo <635388416@qq.com>
2024-04-03 12:43:32 +0800

    3. 无重复字符的最长子串

--- leetCode/normal/LongestSubstringWithoutRepeatingCharacters.java
+++ leetCode/normal/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,5 +1,10 @@
 package normal;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.HashMap;
+
 /**
  * @ClassName: LongestSubstringWithoutRepeatingCharacters
  * @Description: 3. 无重复字符的最长子串
@@ -7,27 +12,57 @@ package normal;
  * @Date: 2023/10/11 13:41
  */
 public class LongestSubstringWithoutRepeatingCharacters {
-    public int lengthOfLongestSubstring(String s) {
-        int ans = 0;
-        // ASCII可打印字符范围为32-126 长度为95
-        // map记录字符上一次出现的序号，从1开始
-        int[] map = new int[95];
-        int l = 1, r = 1;
-        for (; r <= s.length(); r++) {
-            int idx = s.charAt(r - 1) - ' ';
-            // 当出现相同字符时才判断ans，因为长度在相同之前一直增加，减少判断次数
-            if (map[idx] != 0) {
-                // [1, l) [l, r) [r, len+1)
-                // 其中l和r对应的字符相同，求[l, r)的长度
-                ans = Math.max(ans, r - l);
-                // 有可能[map[idx], ..., l, ..., r]，即idx对应的元素的上一次出现在l之前
-                l = Math.max(map[idx] + 1, l);
-                // 在r++之后会变为[l+1, r+1) 窗口内只有一个r
+
+    public String longestSubstring(String s) {
+        int maxLen = 0, start = 0;
+        int[] map = new int[200];
+        Arrays.fill(map, -1);
+        int l = 0, r = 0;
+        // [0, l)[l, r)
+        for (; r < s.length(); r++) {
+            int idx = s.charAt(r);
+            if (r - l > maxLen) {
+                maxLen = r - l;
+                start = l;
+            }
+            if (map[idx] >= 0) {
+                l = Math.max(l, map[idx] + 1);
             }
             map[idx] = r;
         }
-        // r = len + 1
-        ans = Math.max(ans, r - l);
-        return ans;
+        if (r - l > maxLen) {
+            maxLen = r - l;
+            start = l;
+        }
+        return s.substring(start, maxLen + start);
+    }
+
+    // 允许最多重复k次
+    public String longestSubstring(String s, int k) {
+        int maxLen = 0, start = 0;
+        // 记录出现的位置
+        HashMap<Character, Deque<Integer>> map = new HashMap<>();
+        int l = 0, r = 0;
+        // [0, l)[l, r)
+        for (; r < s.length(); r++) {
+            char idx = s.charAt(r);
+            if (r - l > maxLen) {
+                maxLen = r - l;
+                start = l;
+            }
+            if (!map.containsKey(idx)) {
+                map.put(idx, new ArrayDeque<>());
+            }
+            if (map.get(idx).size() == k) {
+                l = Math.max(l, map.get(idx).removeFirst() + 1);
+            }
+            Deque<Integer> deque = map.get(idx);
+            deque.addLast(r);
+        }
+        if (r - l > maxLen) {
+            maxLen = r - l;
+            start = l;
+        }
+        return s.substring(start, maxLen + start);
     }
 }
