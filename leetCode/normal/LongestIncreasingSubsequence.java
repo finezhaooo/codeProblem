d8cfa09f7d98db68c664c83c9e71923562a513e6
finezhaooo <635388416@qq.com>
2024-04-08 20:51:57 +0800

    300. 最长递增子序列

--- leetCode/normal/LongestIncreasingSubsequence.java
+++ leetCode/normal/LongestIncreasingSubsequence.java
@@ -92,4 +92,79 @@ public class LongestIncreasingSubsequence {
         }
         return res;
     }
+
+    // 动态规划 并输出最长子序列
+    public String lengthOfLIS4(int[] nums) {
+        int len = nums.length;
+        // dp[i]表示以num[i]结尾的递增子序列长度
+        int[] dp = new int[len];
+        int[] pre = new int[len];
+        dp[0] = 1;
+        int res = 1;
+        int idx = 0;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < len; i++) {
+            int tmp = 1;
+            for (int j = 0; j < i; j++) {
+                // 当dp[j]+1 > tmp 时才记录前一个
+                if (nums[i] > nums[j] && dp[j] + 1 > tmp) {
+                    pre[i] = j;
+                    tmp = dp[j] + 1;
+                }
+            }
+            dp[i] = tmp;
+            if (tmp == 1) {
+                pre[i] = -1;
+                continue;
+            }
+            if (tmp > res) {
+                idx = i;
+                res = tmp;
+            }
+        }
+        while (idx != -1) {
+            sb.append(nums[idx]);
+            idx = pre[idx];
+        }
+        return sb.reverse().toString();
+    }
+
+    // 二分 并输出最长子序列
+    public String lengthOfLIS5(int[] nums) {
+        int len = nums.length;
+        // tails修改为记录下标而不是值
+        int[] tails = new int[len];
+        // pre记录序列中下标的前一个元素，用于倒推
+        int[] pre = new int[len];
+        // 最长子序列的最后一个元素的下标
+        int idx = 0;
+        int res = 0;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < len; i++) {
+            int num = nums[i];
+            int l = 0, r = res;
+            while (l < r) {
+                int m = (l + r) / 2;
+                if (nums[tails[m]] < num) {
+                    l = m + 1;
+                } else {
+                    r = m;
+                }
+            }
+            // 插入下标
+            tails[l] = i;
+            // 记录当前插入下标的前一个下标
+            // 若l等于0 即当元素没有前一个元素 pre[i] = -1
+            pre[i] = l > 0 ? tails[l - 1] : -1;
+            if (res == r) {
+                res++;
+                idx = i;
+            }
+        }
+        while (idx != -1) {
+            sb.append(nums[idx]);
+            idx = pre[idx];
+        }
+        return sb.reverse().toString();
+    }
 }
